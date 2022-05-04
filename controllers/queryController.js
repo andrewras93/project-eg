e3cba95749169eced34fa9dac2fba138e6dbe014
Andrew James Rasmussen <andrewras93@gmail.com>
2022-05-04 19:33:48 +0200

    Added template for display of a single product and its needed products.

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -3,7 +3,7 @@ const database = require('../models/database.js');
 module.exports = {
     
     handleQuery: (query) => {
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => { // TODO: Research mere omkring Promises.
             database.query(query, (err, result) => {
                 if (err) {
                     return reject(err);
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,27 @@ const express = require('express');
 const router = express.Router(); // TODO: Research præcist hvad express.Router() gør.
 const queryController = require('../controllers/queryController');
 
-// Get all products.
+// GET all products.
 router.get('/', async function (req, res) {
-    const products = await queryController.handleQuery('SELECT * FROM products');
-    console.log(products);
+    const products = await queryController.handleQuery('SELECT id, name, description, price, unit FROM products');
 
     res.render('index', {
-        title: 'Alle Produkter',
-        products: products
+        title: 'Byggemarked Produkter',
+        products: products,
+        product: ''
+    });
+});
+
+// GET one product to show its needed and related products.
+router.get('/produkt/:id', async function (req, res) {
+    const product = await queryController.handleQuery(`SELECT id, name, description, price, unit FROM products WHERE id=${req.params.id}`);
+    const neededProducts = await queryController.handleQuery(`SELECT id, name, description, price, unit FROM products WHERE id !=${req.params.id}`);
+    // const relatedProducts = '';
+
+    res.render('index', {
+        title: 'Byggemarked Produkter',
+        products: neededProducts,
+        product: product[0]
     });
 });
 
