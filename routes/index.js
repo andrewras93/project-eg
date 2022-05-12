8551fb8809720ff9ac6a79315cd2ba10259ba87a
mikkelwoelk <mikkelwolk@gmail.com>
2022-05-12 15:30:32 +0200

    Added routes and handling for signup and login pages

--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,133 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router(); // TODO: Research præcist hvad express.Router() gør.
-const queryController = require('../controllers/queryController');
+const queryController = require("../controllers/queryController");
+const bcrypt = require("bcrypt");
+const { response } = require("express");
 
 let cart;
+let users = [];
 
 // GET all products.
-router.get('/', async function (req, res) {
-    
-    // Check if there's items in the cart.
-    if (req.session.cart) {
-        cart = req.session.cart;
-    } else {
-        req.session.cart = [];
-    }
+router.get("/", async function (req, res) {
+  // Check if there's items in the cart.
+  if (req.session.cart) {
+    cart = req.session.cart;
+  } else {
+    req.session.cart = [];
+  }
 
-    const products = await queryController.handleQuery('SELECT id, name, description, price, unit FROM products');
+  const products = await queryController.handleQuery(
+    "SELECT id, name, description, price, unit FROM products"
+  );
 
-    res.render('index', {
-        title: 'Byggemarked Produkter',
-        products: products,
-        product: '',
-        cart: cart,
-        relatedProducts: ''
-    });
+  res.render("index", {
+    title: "Byggemarked Produkter",
+    products: products,
+    product: "",
+    cart: cart,
+    relatedProducts: "",
+  });
 });
 
-router.get('/alle-produkter', async function (req, res) {
-    const products = await queryController.handleQuery('SELECT * FROM products');
+// Login routers
+router.get("/users", async function (req, res) {
+  const users = await queryController.handleQuery("SELECT * FROM users");
+  res.render("users", {
+    title: "Brugere",
+    users: users,
+  });
+  res.json(users);
+});
 
-    res.json(products);
+router.get("/signup", async function (req, res) {
+  res.render("signup", {
+    title: "Signup",
+  });
+});
+
+router.post("/signup2", async function (req, res) {
+  try {
+    const uname = req.body.username;
+    const hashedPass = bcrypt.hashSync(req.body.password, 10);
+
+    // Insert users into db
+    const user = await queryController.handleQuery(
+      `INSERT INTO users (user, password) VALUES ("${uname}","${hashedPass}")`
+    );
+
+    if (user) {
+      res.redirect("/login");
+    }
+  } catch {
+    res.redirect("/register");
+  }
+  // console.log(users);
+});
+
+router.get("/login", async function (req, res) {
+  res.render("login", {
+    title: "Login",
+  });
+});
+
+router.post("/loginCheck", async function (req, res) {
+  const uname = req.body.username;
+  const password = req.body.password;
+
+  if (uname && password) {
+    const hashedPass = await queryController.handleQuery(
+      `SELECT password FROM users WHERE user="${uname}"`
+    );
+    const pw = hashedPass[0]["password"];
+    console.log(pw);
+    const verified = bcrypt.compareSync(password, pw);
+
+    if (verified) {
+      res.redirect("/");
+    } else {
+      res.send("u fucked up");
+    }
+  }
+});
+
+// Produkt routers
+router.get("/alle-produkter", async function (req, res) {
+  const products = await queryController.handleQuery("SELECT * FROM products");
+
+  res.json(products);
 });
 
 // GET one product to show its needed and related products.
-router.get('/produkt/:id', async function (req, res) {
+router.get("/produkt/:id", async function (req, res) {
+  if (req.session.cart) {
+    cart = req.session.cart;
+  }
 
-    if (req.session.cart) {
-        cart = req.session.cart;
-    }
+  const product = await queryController.handleQuery(
+    `SELECT id, name, description, price, unit FROM products WHERE id=${req.params.id}`
+  );
+  let neededProducts = await queryController.handleQuery(
+    `SELECT id, name, description, price, unit FROM products WHERE id !=${req.params.id}`
+  );
+  const relatedProducts = await queryController.handleQuery(
+    `SELECT id, name, description, price, unit FROM products WHERE id !=${req.params.id}`
+  );
 
-    const product = await queryController.handleQuery(`SELECT id, name, description, price, unit FROM products WHERE id=${req.params.id}`);
-    let neededProducts = await queryController.handleQuery(`SELECT id, name, description, price, unit FROM products WHERE id !=${req.params.id}`);
-    const relatedProducts = await queryController.handleQuery(`SELECT id, name, description, price, unit FROM products WHERE id !=${req.params.id}`);;
-    
-    neededProducts = neededProducts.filter(({ id }) => !cart.some((e) => e.id === id));
-    // Source: https://www.reddit.com/r/learnjavascript/comments/siphq1/compare_two_arrays_with_objects_and_remove/
-    
-    cart.push(product[0]);
+  neededProducts = neededProducts.filter(
+    ({ id }) => !cart.some((e) => e.id === id)
+  );
 
-    res.render('index', {
-        title: 'Byggemarked Produkter',
-        products: neededProducts,
-        product: product[0],
-        cart: cart,
-        relatedProducts: relatedProducts
-    });
+  // Source: https://www.reddit.com/r/learnjavascript/comments/siphq1/compare_two_arrays_with_objects_and_remove/
+
+  cart.push(product[0]);
+
+  res.render("index", {
+    title: "Byggemarked Produkter",
+    products: neededProducts,
+    product: product[0],
+    cart: cart,
+    relatedProducts: relatedProducts,
+  });
 });
 
 module.exports = router;
