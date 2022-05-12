e006c8ce6d6b51f1b90e90d9f5a7b1b0c1fb02fc
mikkelwoelk <mikkelwolk@gmail.com>
2022-05-12 15:28:51 +0200

    Added modules for later use

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
+const session = require("express-session");
 const app = express();
-const index = require('./routes/index.js');
+const index = require("./routes/index.js");
+// const login = require("./routes/login.js");
+const cookie = require("cookie");
+const cookieParser = require("cookie-parser");
 
 // Set the view engine.
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Used for parsing data with POST methods.
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.use(session({
-    secret: 'secret-key',
+app.use(
+  session({
+    secret: "secret-key",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 
-app.use('/', index);
+app.use("/", index);
+// app.use("/users", login);
 
 app.listen(3000);
